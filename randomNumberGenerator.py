080959d760cd2817f177ddbe5fbc4d095c77cbb6
DANIEL MUNENE IKUA <123269657+TheCode69@users.noreply.github.com>
2023-11-27 21:48:32 +0300

    Update randomNumberGenerator.py

--- randomNumberGenerator.py
+++ randomNumberGenerator.py
@@ -1,5 +1,5 @@
 import random
-          
+                           
 # Generate a random integer between 1 and 100 (inclusive)
 random_number = random.randint(1, 100)
 #prints the random number as instructed.
